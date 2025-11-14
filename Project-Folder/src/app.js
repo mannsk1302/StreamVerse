e100475b6c7eb80e1d5550b7fb20b2a1108d6aae
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-11-14 16:18:25 +0530

    Bug fix and api testing

--- Project-Folder/src/app.js
+++ Project-Folder/src/app.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
+  origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
