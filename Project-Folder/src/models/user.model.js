3cafb84478b7f74534a96dd55e60186b3bab0db0
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2026-03-23 03:09:26 +0530

    Usual upgradation and changes.

--- Project-Folder/src/models/user.model.js
+++ Project-Folder/src/models/user.model.js
@@ -62,7 +62,7 @@ userSchema.methods.generateAccessToken = function () {
         {
             _id: this._id
         },
-        process.env.ACCESS_TOKEN_SECRET,
+            process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
         }
@@ -74,7 +74,7 @@ userSchema.methods.generateRefreshToken = function () {
         {
             _id: this._id
         },
-        process.env.REFRESH_TOKEN_SECRET,
+            process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
         }
